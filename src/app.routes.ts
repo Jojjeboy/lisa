66dd29e3f425d00982241634dc8fa24944bd322a
Jojjeboy <karnstamjohannes@gmail.com>
2025-04-23 22:38:45 +0200

    Delete unused files + disable todo submit button on invalid

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
-import { Dashboard } from './app/pages/dashboard/dashboard';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
 import { ListComponent } from './app/pages/list/list.component';
 import { StartComponent } from './app/pages/start/start.component';
@@ -16,13 +13,9 @@ export const appRoutes: Routes = [
             { path: '', component: StartComponent },
             { path: 'category/:id', component: CategoryComponent },
             { path: 'list/:id/:categoryId', component: ListComponent },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation }
         ]
     },
 
-    { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
     { path: '**', redirectTo: '/notfound' }
 ];
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import { Documentation } from './documentation/documentation';
-import { Crud } from './crud/crud';
+
 
 export default [
-    { path: 'crud', component: Crud },
-    { path: 'documentation', component: Documentation },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
